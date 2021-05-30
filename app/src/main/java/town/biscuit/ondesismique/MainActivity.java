3a9bf111e55f51227e2d333f0b6b34d8375ff25a
Tagada <36127788+Tagadda@users.noreply.github.com>
2021-05-30 12:10:42 +0200

    WIP

--- app/src/main/java/town/biscuit/ondesismique/MainActivity.java
+++ app/src/main/java/town/biscuit/ondesismique/MainActivity.java
@@ -12,12 +12,17 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Vibrator vibrator;
+    private Preset[] presets;
+    private RadioGroup rg;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Preset[] presets = {
+        presets = new Preset[]{
                 new Preset(getString(R.string.vibe_infinite), new long[]{0, 1000, 0}, 0),
                 new Preset(getString(R.string.vibe_burst), new long[]{0, 50, 50}, 1),
                 new Preset(getString(R.string.vibe_pulse), new long[]{0, 100, 50}, 0),
@@ -27,36 +32,26 @@ public class MainActivity extends AppCompatActivity {
                 new Preset(getString(R.string.vibe_allpulse), new long[]{0, 300, 50, 200, 50, 100, 50}, 0),
         };
 
-        RadioGroup rg = (RadioGroup) this.findViewById(R.id.radiogroup);
-        for(int i = 0; i < presets.length; i++) {
+        rg = (RadioGroup) this.findViewById(R.id.radiogroup);
+        for (Preset preset : presets) {
             RadioButton rb = new RadioButton(this);
-            rb.setText(presets[i].name);
+            rb.setText(preset.name);
             rg.addView(rb);
         };
 
-        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-
-        Button first = findViewById(R.id.first_button);
-        first.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int id = (int) rg.getCheckedRadioButtonId() - 1;
-                if(id < 0) {
-                    Toast.makeText(getApplicationContext(), getString(R.string.select_reminder), Toast.LENGTH_SHORT).show();
-                } else {
-                    vibrator.vibrate(presets[id].timings, presets[id].repeat);
-                }
-            }
-        });
-
-        Button second = findViewById(R.id.cancel_button);
-        second.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                vibrator.cancel();
-            }
-        });
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
     }
+
+    public void startSismic(View v) {
+        int id = (int) rg.getCheckedRadioButtonId() - 1;
+        if(id < 0) {
+            Toast.makeText(getApplicationContext(), getString(R.string.select_reminder), Toast.LENGTH_SHORT).show();
+        } else {
+            vibrator.vibrate(presets[id].timings, presets[id].repeat);
+        }
+    }
+
+    public void stopSismic(View v) { vibrator.cancel(); }
 
 
 }
